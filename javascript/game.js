25eb3776e120c1349e0129e84743b5b0c179ecdb
unknown <londonvayavong@gmail.com>
2017-11-10 09:30:34 -0500

    4th draft

--- javascript/game.js
+++ javascript/game.js
@@ -1,4 +1,3 @@
-
 var winAmount = 0;
 var lossAmount = 0;
 var targetNumber = Math.floor(Math.random() * 121);
@@ -19,81 +18,77 @@ $("#currentScore").text(amount);
 
 function restartGame() {
 
-targetNumber = Math.floor(Math.random() * 121);
-$("#targetNumber").text(targetNumber);
-Crystal1 = Math.floor(Math.random() * 12);
-Crystal2 = Math.floor(Math.random() * 12);
-Crystal3 = Math.floor(Math.random() * 12);
-Crystal4 = Math.floor(Math.random() * 12);
-counter = 0;
-$("#currentScore").text(amount);
+  targetNumber = Math.floor(Math.random() * 121);
+  $("#targetNumber").text(targetNumber);
+  Crystal1 = Math.floor(Math.random() * 12);
+  Crystal2 = Math.floor(Math.random() * 12);
+  Crystal3 = Math.floor(Math.random() * 12);
+  Crystal4 = Math.floor(Math.random() * 12);
+  counter = 0;
+  $("#currentScore").text(amount);
 
 }
 
 function win() {
 
-	alert("You win!");
-	winAmount ++;
-	$("#winAmount").text(winAmount);
-	restartGame();
+  alert("You win!");
+  winAmount++;
+  $("#winAmount").text(winAmount);
+  restartGame();
 }
 
 function lose() {
 
-	alert("You lose!");
-	lossAmount ++;
-	$("#lossAmount").text(lossAmount);
-	restartGame();
+  alert("You lose!");
+  lossAmount++;
+  $("#lossAmount").text(lossAmount);
+  restartGame();
 }
 
-$('#Crystal1').on ('click', function(){
-    amount = amount + Crystal1;
+$('#Crystal1').on('click', function() {
+  amount = amount + Crystal1;
 
-    $('#currentScore').text(amount);
+  $('#currentScore').text(amount);
 
-        if (amount == targetNumber){
-          win();
-        }
-        else if (amount > targetNumber){
-          lose();
-        }
-  });
+  if (amount == targetNumber) {
+    win();
+  } else if (amount > targetNumber) {
+    lose();
+  }
+});
 
-$('#Crystal2').on ('click', function(){
-    amount = amount + Crystal2;
+$('#Crystal2').on('click', function() {
+  amount = amount + Crystal2;
 
-    $('#currentScore').text(amount);
+  $('#currentScore').text(amount);
 
-        if (amount == targetNumber){
-          win();
-        }
-        else if (amount > targetNumber){
-          lose();
-        }
-  });
+  if (amount == targetNumber) {
+    win();
+  } else if (amount > targetNumber) {
+    lose();
+  }
+});
 
-$('#Crystal3').on ('click', function(){
-    amount = amount + Crystal3;
+$('#Crystal3').on('click', function() {
+  amount = amount + Crystal3;
 
-    $('#currentScore').text(amount);
+  $('#currentScore').text(amount);
 
-        if (amount == targetNumber){
-          win();
-        }
-        else if (amount> targetNumber){
-          lose();
-        }
-  });
+  if (amount == targetNumber) {
+    win();
+  } else if (amount > targetNumber) {
+    lose();
+  }
+});
 
-$('#Crystal4').on ('click', function(){
-    amount = amount + Crystal4;
+$('#Crystal4').on('click', function() {
+  amount = amount + Crystal4;
 
-    $('#currentScore').text(amount);
+  $('#currentScore').text(amount);
 
-        if (amount == targetNumber){
-          win();
-        }
-        else if (amount > targetNumber){
-          lose();
-        }
-  });
+  if (amount == targetNumber) {
+    win();
+  } else if (amount > targetNumber) {
+    lose();
+  }
+});
